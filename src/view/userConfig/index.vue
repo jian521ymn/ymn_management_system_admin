52d31123d7061f5386ba94143f272544fb84b824
jian <1475186481@qq.com>
2022-07-09 01:06:41 +0800

    ceshi1

--- src/view/userConfig/index.vue
+++ src/view/userConfig/index.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-    <div class="search" style="textAlign:left">
+    <div class="search tl">
       <el-form :inline="true" :model="formData">
         <el-form-item label="选择所属角色">
-          <el-input v-model="formData.userRole" placeholder="选择所属角色" clearable></el-input>
+          <el-input
+            v-model="formData.userRole"
+            placeholder="选择所属角色"
+            clearable
+          ></el-input>
         </el-form-item>
         <el-form-item label="查询用户名称">
-          <el-input v-model="formData.userName" placeholder="输入用户名称" clearable></el-input>
+          <el-input
+            v-model="formData.userName"
+            placeholder="输入用户名称"
+            clearable
+          ></el-input>
         </el-form-item>
-        <div style="textAlign:right">
+        <div class="tr">
           <el-form-item>
-          <el-button type="primary" @click="onSubmit">查询</el-button>
-        </el-form-item>
+            <el-button type="primary" @click="onSubmit">查询</el-button>
+          </el-form-item>
         </div>
       </el-form>
     </div>
@@ -21,44 +29,54 @@
         </el-table-column>
         <el-table-column prop="name" label="姓名" width="180">
         </el-table-column>
-        <el-table-column prop="address" label="地址">
-        </el-table-column>
+        <el-table-column prop="address" label="地址"> </el-table-column>
       </el-table>
+      <el-pagination
+        class="tr"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page.sync="this.page.pageNum"
+        :page-size="this.page.pageSize"
+        layout="total, prev, pager, next"
+        :total="this.page.total"
+      >
+      </el-pagination>
     </div>
   </div>
-
 </template>
 
 <script>
-import http from '../../utils/http';
+import http from "../../utils/http";
 export default {
   name: "userConfig",
   data() {
     return {
-      formData:{},
+      formData: {},
       tableData: [],
-      page:{
-        pageSize:10,
-        pageNum:1,
-        total:0
-      }
-    }
+      page: {
+        pageSize: 10,
+        pageNum: 1,
+        total: 0,
+      },
+    };
   },
-  created(){
-    this.getList()
+  created() {
+    this.getList();
   },
-  methods:{
-    getList(){
-      http.get('user/list')
-        .then(res=>{
-          const {list,total,pageSize,pageNum} = res?.data || {}
-          this.tableData = list || [];
-          this.page ={ total,pageSize,pageNum }
-        })
+  methods: {
+    getList() {
+      http.get("user/list").then((res) => {
+        const { list, total, pageSize, pageNum } = res?.data || {};
+        this.tableData = list || [];
+        this.page = { total, pageSize, pageNum };
+      });
     },
-    onSubmit(value){
-      console.log(value,this.formData);
-    }
-  }
-}
+    handleCurrentChange(pageNum){
+      console.log(pageNum);
+    },
+    onSubmit(value) {
+      console.log(value, this.formData);
+    },
+  },
+};
 </script>
